0b718fb4628be5e74e464a9984e22e20c1b617a5
Soufiane Chahid <schahid.001@gmail.com>
2025-07-28 16:20:05 +0100

    using jinja2

--- hooks/web/web-precommit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Run pre-commit using the nested config, but ONLY for the staged files
-echo "🔁 Running nested pre-commit config for web project..."
-
-pre-commit run --config ./.pre-commit-config.yaml
